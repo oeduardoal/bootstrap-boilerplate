6f1ad94397031b8c82e95ea59fd0ca1a28576040
Eduardo Almeida <eduardoalmeida258@gmail.com>
2017-06-12 23:17:57 -0300

    Browsersync in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,9 @@ var uglify = require('gulp-uglify'),
   source = require('vinyl-source-stream'),
   buffer = require('vinyl-buffer');
 
+// Browser SYNC
+var browserSync = require('browser-sync').create();
+
 // Task For Compile SCSS
 gulp.task('sass', function(){
   var files = [
@@ -22,6 +25,7 @@ gulp.task('sass', function(){
     .pipe(concat('main.min.css'))
     .pipe(cssnano({ zindex: false }))
     .pipe(gulp.dest('dist/css'))
+    .pipe(browserSync.stream())
 })
 
 gulp.task('js', function(){
@@ -32,10 +36,19 @@ gulp.task('js', function(){
   .pipe(source('main.min.js'))
   .pipe(buffer())
   .pipe(uglify())
-  .pipe(gulp.dest('dist/js/'));
+  .pipe(gulp.dest('dist/js/'))
+  .pipe(browserSync.stream());
 });
 
-gulp.task('default', ['sass', 'js'], function (){
+// Task BrowserSYNC
+gulp.task('server', ['sass', 'js'], function() {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
   gulp.watch('assets/sass/*.scss', ['sass']);
   gulp.watch('assets/js/*.js', ['js']);
+  gulp.watch("*").on('change', browserSync.reload);
 })
+gulp.task('default', ['server'])
